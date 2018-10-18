1ed8bd35a1982178b1fafbd11d45c8a9fae4ccbf
TFGhub <44195025+TFGhub@users.noreply.github.com>
2018-10-18 14:40:42 +0300

    update index
    removing meta names

--- index.php
+++ index.php
@@ -7,8 +7,7 @@
 <!-- Meta Tags -->
 <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
 <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
-<meta name="description" content="Consulting Finance Corporate Business - ConsultingPro HTML Template" />
-<meta name="keywords" content="consulting,finance,advisor,consultant,business,service,insurance,agency" />
+
 <meta name="author" content="ThemeMascot" />
 
 <!-- Page Title -->
