8f3c63817f33ee92b11056535a6f2addf7f765e8
TGFhub <muthuicris@gmail.com>
2018-10-28 11:34:03 +0300

    changes by cmk

---
+++ about.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+
+<head>
+
+    <!-- Meta Tags -->
+    <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+    <meta name="description" content="The Future Generation"/>
+    <meta name="keywords" content="The Future Generation"/>
+    <meta name="author" content="TheFutureGeneration"/>
+
+    <!-- Page Title -->
+    <title>The Future Generation</title>
+
+    <!-- Favicon and Touch Icons -->
+    <link href="images/favicon.png" rel="shortcut icon" type="image/png">
+    <link href="images/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="images/apple-touch-icon-72x72.png" rel="apple-touch-icon" sizes="72x72">
+    <link href="images/apple-touch-icon-114x114.png" rel="apple-touch-icon" sizes="114x114">
+    <link href="images/apple-touch-icon-144x144.png" rel="apple-touch-icon" sizes="144x144">
+
+    <!-- Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="css/jquery-ui.min.css" rel="stylesheet" type="text/css">
+    <link href="css/animate.css" rel="stylesheet" type="text/css">
+    <link href="css/css-plugin-collections.css" rel="stylesheet"/>
+    <!-- CSS | menuzord megamenu skins -->
+    <link href="css/menuzord-megamenu.css" rel="stylesheet"/>
+    <link id="menuzord-menu-skins" href="css/menuzord-skins/menuzord-border-bottom.css" rel="stylesheet"/>
+    <!-- CSS | Main style file -->
+    <link href="css/style-main.css" rel="stylesheet" type="text/css">
+    <!-- CSS | Preloader Styles -->
+    <link href="css/preloader.css" rel="stylesheet" type="text/css">
+    <!-- CSS | Custom Margin Padding Collection -->
+    <link href="css/custom-bootstrap-margin-padding.css" rel="stylesheet" type="text/css">
+    <!-- CSS | Responsive media queries -->
+    <link href="css/responsive.css" rel="stylesheet" type="text/css">
+    <!-- CSS | Style css. This is the file where you can place your own custom css code. Just uncomment it and use it. -->
+    <!--<link href="css/style.css" rel="stylesheet" type="text/css">-->
+
+    <!-- Revolution Slider 5.x CSS settings -->
+    <link href="js/revolution-slider/css/settings.css" rel="stylesheet" type="text/css"/>
+    <link href="js/revolution-slider/css/layers.css" rel="stylesheet" type="text/css"/>
+    <link href="js/revolution-slider/css/navigation.css" rel="stylesheet" type="text/css"/>
+
+    <!-- CSS | Theme Color -->
+    <link href="css/colors/theme-skin-color-set4.css" rel="stylesheet" type="text/css">
+
+    <!-- external javascripts -->
+    <script src="js/jquery-2.2.4.min.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- JS | jquery plugin collection for this theme -->
+    <script src="js/jquery-plugin-collection.js"></script>
+
+    <!-- Revolution Slider 5.x SCRIPTS -->
+    <script src="js/revolution-slider/js/jquery.themepunch.tools.min.js"></script>
+    <script src="js/revolution-slider/js/jquery.themepunch.revolution.min.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body class="">
+<div id="wrapper" class="clearfix">
+    <!-- preloader -->
+    <!--<div id="preloader">
+      <div id="spinner">
+        <img alt="" src="../images/preloaders/5.gif">
+      </div>
+      <div id="disable-preloader" class="btn btn-default btn-sm">Disable Preloader</div>
+    </div>-->
+
+    <!-- Header -->
+    <?php include "includes/header.php" ?>
+
+    <!-- Start main-content -->
+    <div class="main-content">
+
+        <!-- Section: inner-header -->
+        <section class="inner-header divider parallax layer-overlay overlay-white-8" data-bg-img="images/bg/bg4.jpg">
+                  
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-11 col-xs-offset-1 border-4px p-20">
+                        <h4>ABOUT US</h4>
+<p>The future generation is a national Christian humanitarian organization, dedicated to working with men, women children and youths to reach their full potential by addressing their root causes of poverty and injustice. 
+We partner with communities, the Government, sponsors, donors and corporates to realize our strategy, our goal, of building and Developing An Empowered Generation 
+Through our strategy, we envision children living and enjoying life in all its fullness. Youth and women accessing opportunities stable and living just life. To this end we work with partners to improve livelihoods and resilience, education & child protection, health, nutrition and HIV/Aids, humanitarian emergency affairs  leadership and governance and access to water, sanitation and hygiene (WASH) for children and their families. 
+Through our advocacy work, we enable the voices of youth, women and children and their communities to reach decision-makers, who have power to change unjust policies and practices. 
+
+The Future Generation has program reach in Kenya, with Area Programs – long – term and short Term development strategic program with target group being men, youth women and children.
+The future generation serves all people regardless of religion, race, ethnicity or gender.</p>
+<hr>
+                        <h4 class="mt-0 pt-9">WHO WE ARE  </h4>
+                        <p>he Future Generation is a non-governmental organization founded in 2018 committed, to eradicate poverty through empowering the youth, women and children to be self-reliance capable of building resilience of coping with the current situation within their locality. </p>
+                        <hr>
+
+                       <h4 class="mt-0 pt-9"> OBJECTIVE</h4>
+                       <ul class="list-icon theme-colored">
+  
+          <li><i class="fa fa-arrow-right"></i> To ensure disadvantaged youth and women acquire life skills, relevant market skills, savings education and small business development. </li>
+          <li><i class="fa fa-arrow-right"></i>To ensure that women are empowered to achieve their life goals and have stable sustainable life through skilled training.</li>
+          
+
+                   
+</ul>
+<hr>
+            <h4>VISION</h4>
+            <p>Empowered communities capable of uplifting their living standards holistically through the transforming. </p>
+
+           <hr>
+<h4>MISSION</h4>
+
+<p>Empowering the society by impacting to them social, economic skill  and commitment in holistic and sustainable development among the poor and marginalized communities through advocacy, capacity building and promoting physical, social, economic, and Spiritual well-being.
+</p>
+<hr>
+
+
+                    </div>
+                   
+                </div>
+            </div>
+        </section>
+    </section>
+    </div>
+    <!-- end main-content -->
+
+    <!-- Footer -->
+    <?php include "includes/footer.php" ?>
+
+    <a class="scrollToTop" href="#"><i class="fa fa-angle-up"></i></a>
+</div>
+<!-- end wrapper -->
+
+<!-- Footer Scripts -->
+<!-- JS | Custom script for all pages -->
+<script src="js/custom.js"></script>
+
+</body>
+
+</html> 
---
+++ childprotection.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+
+<head>
+
+    <!-- Meta Tags -->
+    <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+    <meta name="description" content="The Future Generation"/>
+    <meta name="keywords" content="The Future Generation"/>
+    <meta name="author" content="TheFutureGeneration"/>
+
+    <!-- Page Title -->
+    <title>The Future Generation</title>
+
+    <!-- Favicon and Touch Icons -->
+    <link href="images/favicon.png" rel="shortcut icon" type="image/png">
+    <link href="images/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="images/apple-touch-icon-72x72.png" rel="apple-touch-icon" sizes="72x72">
+    <link href="images/apple-touch-icon-114x114.png" rel="apple-touch-icon" sizes="114x114">
+    <link href="images/apple-touch-icon-144x144.png" rel="apple-touch-icon" sizes="144x144">
+
+    <!-- Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="css/jquery-ui.min.css" rel="stylesheet" type="text/css">
+    <link href="css/animate.css" rel="stylesheet" type="text/css">
+    <link href="css/css-plugin-collections.css" rel="stylesheet"/>
+    <!-- CSS | menuzord megamenu skins -->
+    <link href="css/menuzord-megamenu.css" rel="stylesheet"/>
+    <link id="menuzord-menu-skins" href="css/menuzord-skins/menuzord-border-bottom.css" rel="stylesheet"/>
+    <!-- CSS | Main style file -->
+    <link href="css/style-main.css" rel="stylesheet" type="text/css">
+    <!-- CSS | Preloader Styles -->
+    <link href="css/preloader.css" rel="stylesheet" type="text/css">
+    <!-- CSS | Custom Margin Padding Collection -->
+    <link href="css/custom-bootstrap-margin-padding.css" rel="stylesheet" type="text/css">
+    <!-- CSS | Responsive media queries -->
+    <link href="css/responsive.css" rel="stylesheet" type="text/css">
+    <!-- CSS | Style css. This is the file where you can place your own custom css code. Just uncomment it and use it. -->
+    <!--<link href="css/style.css" rel="stylesheet" type="text/css">-->
+
+    <!-- Revolution Slider 5.x CSS settings -->
+    <link href="js/revolution-slider/css/settings.css" rel="stylesheet" type="text/css"/>
+    <link href="js/revolution-slider/css/layers.css" rel="stylesheet" type="text/css"/>
+    <link href="js/revolution-slider/css/navigation.css" rel="stylesheet" type="text/css"/>
+
+    <!-- CSS | Theme Color -->
+    <link href="css/colors/theme-skin-color-set4.css" rel="stylesheet" type="text/css">
+
+    <!-- external javascripts -->
+    <script src="js/jquery-2.2.4.min.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- JS | jquery plugin collection for this theme -->
+    <script src="js/jquery-plugin-collection.js"></script>
+
+    <!-- Revolution Slider 5.x SCRIPTS -->
+    <script src="js/revolution-slider/js/jquery.themepunch.tools.min.js"></script>
+    <script src="js/revolution-slider/js/jquery.themepunch.revolution.min.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body class="">
+<div id="wrapper" class="clearfix">
+    <!-- preloader -->
+    <!--<div id="preloader">
+      <div id="spinner">
+        <img alt="" src="../images/preloaders/5.gif">
+      </div>
+      <div id="disable-preloader" class="btn btn-default btn-sm">Disable Preloader</div>
+    </div>-->
+
+    <!-- Header -->
+    <?php include "includes/header.php" ?>
+
+    <!-- Start main-content -->
+    <div class="main-content">
+
+        <!-- Section: inner-header -->
+        <section class="inner-header divider parallax layer-overlay overlay-white-8" data-bg-img="images/bg/tfg3.png">
+            <div class="container pt-60 pb-60">
+                <!-- Section Content -->
+                <div class="section-content">
+                    <div class="row">
+                        <div class="col-md-12 text-center">
+                            <h3 class="title">Child protection and Inclusion</h3>
+                            <ol class="breadcrumb text-center text-black mt-10">
+                                <li><a href="index.php">Home</a></li>
+                                
+                                <li class="active text-theme-colored">CHild protection</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+ <section class="inner-header divider parallax layer-overlay overlay-white-8" data-bg-img="images/bg/bg4.jpg">
+        <section>
+            <div class="container" >
+                <div class="row">
+                    <div class="col-xs-10 col-xs-offset-2 border-4px p-20">
+                        <h4 class="mt-0 pt-9">Child protection and inclusion</h4>
+                        <p><h5>Every child has the right to grow and thrive up in a safe and inclusive environment </h5></p>
+                        <hr>
+
+                       <h4 class="mt-0 pt-9"> Explore our child protection and inclusion initiatives</h4>
+                       <ul class="list-icon theme-colored">
+  
+          <li><i class="fa fa-arrow-right"></i> Child protection </li>
+          <li><i class="fa fa-arrow-right"></i>Social Inclusion</li>
+          <li><i class="fa fa-arrow-right"></i>Gender Equality</li>
+          <li><i class="fa fa-arrow-right"></i>Education</li>
+
+          <li><i class="fa fa-arrow-right"></i>adolescent development</li> 
+          
+</ul>
+            <h4>EVERY CHILD HAS THE RIGHT TO LEARN</h4>
+            <p>The future generation works to support quality learning for every girl and boy, especially those in greatest danger of vulnerability. </p>
+            <h4>Explore our education initiative</h4>
+
+<ul class="list-icon theme-colored">
+  
+          <li><i class="fa fa-arrow-right"></i> Child living with disability  </li>
+          <li><i class="fa fa-arrow-right"></i>Early child development     </li>
+          <li><i class="fa fa-arrow-right"></i>gender equality </li>
+          <li><i class="fa fa-arrow-right"></i>Education</li>
+
+          <li><i class="fa fa-arrow-right"></i>innovation in education </li> 
+          
+</ul>
+<h4>GENDER </h4>
+<h5>Every girl has the right to fulfil her potential</h5>
+<p>The future generation works to empower women and girls, and to ensure their full participation in political social and economic systems.
+Explore our work to advance gender equality
+</p>
+<h4>Innovation for children</h4>
+<h5>Innovation to drive results for every child</h5>
+<p>The future generation works with partners in every sector to co-create innovative solutions that accelerate progress for children and young people.
+Explore how our innovative solution changes 
+lives 
+</p>
+
+                    </div>
+                   
+                </div>
+            </div>
+        </section>
+    </section>
+    </div>
+    <!-- end main-content -->
+
+    <!-- Footer -->
+    <?php include "includes/footer.php" ?>
+
+    <a class="scrollToTop" href="#"><i class="fa fa-angle-up"></i></a>
+</div>
+<!-- end wrapper -->
+
+<!-- Footer Scripts -->
+<!-- JS | Custom script for all pages -->
+<script src="js/custom.js"></script>
+
+</body>
+
+</html>
