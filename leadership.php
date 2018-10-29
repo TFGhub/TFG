ae9be377484e6ae0634072eb5eac466ab9c18e7f
TGFhub <muthuicris@gmail.com>
2018-10-29 17:25:44 +0300

    changes by cmk

---
+++ leadership.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+
+<head>
+
+    <!-- Meta Tags -->
+    <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+    <meta name="description" content="The Future Generation"/>
+    <meta name="keywords" content="The Future Generation"/>
+    <meta name="author" content="TheFutureGeneration"/>
+
+    <!-- Page Title -->
+    <title>The Future Generation</title>
+
+    <!-- Favicon and Touch Icons -->
+    <link href="images/favicon.png" rel="shortcut icon" type="image/png">
+    <link href="images/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="images/apple-touch-icon-72x72.png" rel="apple-touch-icon" sizes="72x72">
+    <link href="images/apple-touch-icon-114x114.png" rel="apple-touch-icon" sizes="114x114">
+    <link href="images/apple-touch-icon-144x144.png" rel="apple-touch-icon" sizes="144x144">
+
+    <!-- Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="css/jquery-ui.min.css" rel="stylesheet" type="text/css">
+    <link href="css/animate.css" rel="stylesheet" type="text/css">
+    <link href="css/css-plugin-collections.css" rel="stylesheet"/>
+    <!-- CSS | menuzord megamenu skins -->
+    <link href="css/menuzord-megamenu.css" rel="stylesheet"/>
+    <link id="menuzord-menu-skins" href="css/menuzord-skins/menuzord-border-bottom.css" rel="stylesheet"/>
+    <!-- CSS | Main style file -->
+    <link href="css/style-main.css" rel="stylesheet" type="text/css">
+    <!-- CSS | Preloader Styles -->
+    <link href="css/preloader.css" rel="stylesheet" type="text/css">
+    <!-- CSS | Custom Margin Padding Collection -->
+    <link href="css/custom-bootstrap-margin-padding.css" rel="stylesheet" type="text/css">
+    <!-- CSS | Responsive media queries -->
+    <link href="css/responsive.css" rel="stylesheet" type="text/css">
+    <!-- CSS | Style css. This is the file where you can place your own custom css code. Just uncomment it and use it. -->
+    <!--<link href="css/style.css" rel="stylesheet" type="text/css">-->
+
+    <!-- Revolution Slider 5.x CSS settings -->
+    <link href="js/revolution-slider/css/settings.css" rel="stylesheet" type="text/css"/>
+    <link href="js/revolution-slider/css/layers.css" rel="stylesheet" type="text/css"/>
+    <link href="js/revolution-slider/css/navigation.css" rel="stylesheet" type="text/css"/>
+
+    <!-- CSS | Theme Color -->
+    <link href="css/colors/theme-skin-color-set4.css" rel="stylesheet" type="text/css">
+
+    <!-- external javascripts -->
+    <script src="js/jquery-2.2.4.min.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- JS | jquery plugin collection for this theme -->
+    <script src="js/jquery-plugin-collection.js"></script>
+
+    <!-- Revolution Slider 5.x SCRIPTS -->
+    <script src="js/revolution-slider/js/jquery.themepunch.tools.min.js"></script>
+    <script src="js/revolution-slider/js/jquery.themepunch.revolution.min.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body class="">
+<div id="wrapper" class="clearfix">
+    <!-- preloader -->
+    <!--<div id="preloader">
+      <div id="spinner">
+        <img alt="" src="../images/preloaders/5.gif">
+      </div>
+      <div id="disable-preloader" class="btn btn-default btn-sm">Disable Preloader</div>
+    </div>-->
+
+    <!-- Header -->
+    <?php include "includes/header.php" ?>
+
+    <!-- Start main-content -->
+    <div class="main-content">
+
+        <!-- Section: inner-header -->
+        <section class="inner-header divider parallax layer-overlay overlay-white-8" data-bg-img="images/bg/bg4.jpg">
+                  
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-11 col-xs-offset-1 border-4px p-20">
+                        <h4>Leadership and Governance </h4>
+<p>This program is primarily concerned with strengthening meaningful participation of youth and women leaders in decision making spaces at National and County levels . The program focuses on.</p>
+<hr>
+                        
+            <h4 class="mt-0 pt-9">The program focuses on </h4>
+                       <ul class="list-icon theme-colored">
+  
+          <li><i class="fa fa-arrow-right"></i> Strengthening and expanding the pipeline of women leaders to enter political and non-political decision making spaces. </li>
+          <li><i class="fa fa-arrow-right"></i>Supporting women leaders already in political and non-political decision making spaces to better articulate and champion women’s needs and priorities. Specifically in the formulation of gender responsive policies, legislation, budgets and projects.</li>
+          <li><i class="fa fa-arrow-right"></i> Strengthening capacity of youth and women led groups to carry out advocacy and engage with duty bearers around issues that are priority for women.</li>
+
+                   
+</ul>
+<hr>
+
+<hr>
+
+
+                    </div>
+                   
+                </div>
+            </div>
+        </section>
+    </section>
+    </div>
+    <!-- end main-content -->
+
+    <!-- Footer -->
+    <?php include "includes/footer.php" ?>
+
+    <a class="scrollToTop" href="#"><i class="fa fa-angle-up"></i></a>
+</div>
+<!-- end wrapper -->
+
+<!-- Footer Scripts -->
+<!-- JS | Custom script for all pages -->
+<script src="js/custom.js"></script>
+
+</body>
+
+</html> 
